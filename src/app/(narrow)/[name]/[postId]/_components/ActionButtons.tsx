52e441de133875aefc8e612d59e9a5f65c24746e
10012 <nas7062@naver.com>
2025-11-04 19:32:51 +0900

    style : hover시 애니메이션 적용

--- src/app/(narrow)/[name]/[postId]/_components/ActionButtons.tsx
+++ src/app/(narrow)/[name]/[postId]/_components/ActionButtons.tsx
@@ -3,12 +3,12 @@ import { Heart, Share2 } from "lucide-react";
 export default function ActionButtons() {
   return (
     <div className="flex flex-col h-44 w-20 rounded-4xl justify-between items-center py-2 bg-gray-100">
-      <div className="w-14 h-14 bg-white rounded-full flex justify-center items-center border border-gray-200">
-        <Heart className="w-8 h-8" />
+      <div className="w-14 h-14 bg-white rounded-full flex justify-center items-center border border-gray-200 cursor-pointer group hover:border-gray-800">
+        <Heart className="w-8 h-8 group-hover:fill-black" />
       </div>
       <p className="text-lg font-semibold">1</p>
-      <div className="w-14 h-14 bg-white rounded-full flex justify-center items-center border border-gray-200">
-        <Share2 className="w-8 h-8" />
+      <div className="w-14 h-14 bg-white rounded-full flex justify-center items-center border border-gray-200 group hover:border-gray-800 cursor-pointer">
+        <Share2 className="w-8 h-8  group-hover:fill-black" />
       </div>
     </div>
   );

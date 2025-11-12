06c2a5d54839140af48a20d56e9c7bb244c27780
nas7062 <nas7062@naver.com>
2025-11-12 14:12:15 +0900

    feat : 검색 결과 불러와 갯수 표시

--- src/app/(wide)/search/page.tsx
+++ src/app/(wide)/search/page.tsx
@@ -1,21 +1,25 @@
-import { use } from "react";
 import SearchInput from "./_components/SearchInput";
 import SinglePostList from "../../_components/SinglePostList";
 
-export default function SearchPage({
+import { getSearchPost } from "../../_lib/getSearchPost";
+import { IPost } from "../../_components/PostList";
+
+export default async function SearchPage({
   searchParams,
 }: {
   searchParams: Promise<{ q?: string | string[] }>;
 }) {
-  const { q: raw } = use(searchParams);
+  const { q: raw } = await searchParams;
   const q = Array.isArray(raw) ? raw[0] : (raw ?? "");
+  const posts: IPost[] = await getSearchPost({ q });
 
   return (
     <main className="flex flex-col justify-center mx-auto  gap-8">
       <SearchInput q={q} />
       <div className="flex justify-center">
         <h3 className="text-2xl text-gray-600">
-          총 10000건의 포스트를 찾았습니다.
+          총 <strong className="text-gray-800 text-3xl">{posts.length}</strong>
+          건의 포스트를 찾았습니다.
         </h3>
       </div>
       <div className="flex gap-2">
@@ -24,7 +28,7 @@ export default function SearchPage({
         </p>
         <p className="cursor-pointer">최신순</p>
       </div>
-      <SinglePostList q={q} />
+      <SinglePostList posts={posts} />
     </main>
   );
 }
--- src/app/_components/SinglePostList.tsx
+++ src/app/_components/SinglePostList.tsx
@@ -1,10 +1,7 @@
-import { getSearchPost } from "../_lib/getSearchPost";
 import { IPost } from "./PostList";
 import SinglePost from "./SinglePost";
 
-export default async function SinglePostList(q: { q?: string }) {
-  const posts: IPost[] = await getSearchPost(q);
-  console.log(posts);
+export default async function SinglePostList({ posts }: { posts: IPost[] }) {
   return (
     <div className="flex flex-wrap justify-center gap-x-4 gap-y-8">
       {posts.map((post) => (

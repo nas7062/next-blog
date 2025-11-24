ac746dcc786adc4cbb184c9d53e5c1050ec43f77
nas7062 <nas7062@naver.com>
2025-11-24 13:56:54 +0900

    fix : build오류 해결

--- src/app/api/supabase.ts
+++ src/app/api/supabase.ts
@@ -2,6 +2,11 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "../type/supabase";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey); // 클라이언트 생성
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+if (!supabaseUrl) {
+  throw new Error("supabaseUrl is required.");
+}
+if (!supabaseAnonKey) {
+  throw new Error("supabaseAnonKey is required.");
+}
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey); // 클라이언트 생성
